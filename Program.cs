12e4cbc41a6b99ea0df18d372dda02e2ec7a6505
alarakoksal <alarakoksal75@gmail.com>
2026-05-05 01:55:45 +0300

    final working config

--- Program.cs
+++ Program.cs
@@ -2,42 +2,43 @@ using ea_makina.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=app.db"));
-builder.Configuration.AddEnvironmentVariables();
 
+// ❗ SADECE BU KALACAK
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TestDb"));
+
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("TestDb");
 
 var app = builder.Build();
+
+// ✅ Seed data
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-    if (!db.Products.Any())
-{
-    db.Products.Add(new Product
-    {
-        Name = "Test Ürün",
-        Description = "Test açıklama",
-        Price = 0,
-        ImageUrl = ""
-    });
 
-    db.SaveChanges();
-}
+    if (!db.Products.Any())
+    {
+        db.Products.Add(new Product
+        {
+            Name = "Test Ürün",
+            Description = "Test açıklama",
+            Price = 0,
+            ImageUrl = ""
+        });
+
+        db.SaveChanges();
+    }
 }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
